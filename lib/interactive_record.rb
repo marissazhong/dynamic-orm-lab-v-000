6a13631e7ca63ad72c48e732ff86b163c9ac6b75
Marissa Zhong <mwz2101@columbia.edu>
2018-07-08 20:12:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,24 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each {|row| column_names << row["name"]}
+    column_names.compact
+  end
+
+  def initialize(attributes={})
+    attributes.each {|k,v| self.send("#{k}=",v)}
+  end
+
 end
